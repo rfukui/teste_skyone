63b6869bd180c1a80bbd5c3a3b889e451b473140
fukui <r.fukui@gmail.com>
2018-08-30 19:04:28 -0300

    incluido arquivo de output, servico funcional, falta readme

--- main.py
+++ main.py
@@ -7,84 +7,126 @@ umax = 0
 args = {
     0: "input1.txt",
     1: "5",
-    2: "10"
+    2: "10",
+    3: "output.txt",
 }
 filename = "input1.txt"
-users = {}
+servers = []
+output_filename = "output.txt"
 
 
-def add_user(_id):
-    users[str(_id)] = ttask
+def get_free_server():
+    server_num = 1
+    for i, server in enumerate(servers):
+        if len(servers[i]["users"]) < umax:
+            return servers[i]["id"]
+    sid = str(time())
+    servers.append({"id": sid, "users": []})
+    return sid
 
 
-def remove_users(_users):
-    for user in _users:
-        users.pop(user)
+def add_user(uid, sid):
+    for server in servers:
+        if server["id"] == sid:
+            server["users"].append({"id": uid, "tasks": ttask})
 
 
-def open_file():
+def remove_servers():
+    server_no_users = []
+    for server in servers:
+        if len(server["users"]) == 0:
+            server_no_users.append(server)
+    for server in server_no_users:
+        servers.remove(server)
+
+
+def remove_users():
+    for i, server in enumerate(servers):
+        user_zero_procs = []
+        for k, user in enumerate(servers[i]["users"]):
+            servers[i]["users"][k]["tasks"] -= 1
+            if servers[i]["users"][k]["tasks"] == 0:
+                user_zero_procs.append(servers[i]["users"][k])
+        for user in user_zero_procs:
+            servers[i]["users"].remove(user)
+
+
+def read_file():
     f = open(filename).readlines()
-    print(f)
     return f
 
 
-def get_qtd_all_users():
-    return len(users)
+def add_users(qtd_user):
 
-
-def get_qtd_servers(qtd_all_users):
-    return ceil(qtd_all_users / umax)
-
-
-def get_output():
-    total_users = get_qtd_all_users()
-    users_by_servers = []
-    for x in range(get_qtd_servers(get_qtd_all_users())):
-        if total_users <= umax:
-            users_by_servers.append(str(total_users))
-        else:
-            users_by_servers.append(str(umax))
-        total_users -= umax
-
-    return ",".join(users_by_servers)
-
-
-def adjust_users(qtd_user):
-    total_users = 0
-    user_zero_procs = []
-    for user in users:
-        users[user] -= 1
-        if users[user] == 0:
-            user_zero_procs.append(user)
-
-    remove_users(user_zero_procs)
     for x in range(0, qtd_user):
-        add_user(_id=time())
-    
-    print(get_output())
+        add_user(str(time()), get_free_server())
+
+
+def get_servers_snapshot():
+    output = []
+    for server in servers:
+        output.append(str(len(server["users"])))
+    if len(output):
+        return ",".join(output) + "\n"
+    return "0\n"
+
+
+def get_qtd_servers():
+    return len(servers)
+
+
+def add_line(line, file):
+
+    file.write(line)
+
+
+def open_file():
+    return open(output_filename, "w")
 
 
 def main():
     try:
-        file = open_file()
+        file = read_file()
     except FileNotFoundError as e:
         raise Exception("file not found")
 
+    output_file = open_file()
+
     for qtd_user in file:
+        remove_users()
+        remove_servers()
         try:
             usr = int(qtd_user)
-            adjust_users(usr)
+            add_users(usr)
         except ValueError as e:
             pass
-        
-    while get_qtd_all_users():
-        adjust_users(0)
+        add_line(get_servers_snapshot(), output_file)
+    while get_qtd_servers():
+        remove_users()
+        remove_servers()
+        add_line(get_servers_snapshot(), output_file)
+
 
 if __name__ == '__main__':
+    try:
+        if sys.argv[1] in ("help", "?", "--help", "-help"):
+            print("""
+./main.py
+executa o sistema para o arquivo imput1.txt com ttasks =5, umax = 10 e gera o arquivo
+de saida output.txt
+para mudar cada um dos items a sequencia é
+./main.py <input file> <ttask> <umax> <outputfilename>
+a ordem deve ser preservada
+        """)
+            sys.exit()
+    except IndexError as e:
+        pass
+    
 
     for k, arg in enumerate(sys.argv[1:]):
         args[k] = arg
     filename = args[0]
     ttask = int(args[1])
     umax = int(args[2])
+    output_filename = args[3]
     main()
